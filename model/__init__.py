54691cf007788b22244ad584a65edbe6fc6b5a74
Mehralizada <gulanemehrelizade97@gmail.com>
2024-06-22 14:33:41 +0000

    file updated

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,3 @@
-# persistence/__init__.py
-
-from .data_manager import DataManager
-from .persistence_manager import PersistenceManager
+# model/__init__.py
+from .users import User
+from .base import Base
