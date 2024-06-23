7d02f6183a9798574c7b2d444019f4d4e88e089f
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-23 10:27:19 +0000

    test_state.py

--- test/test_state.py
+++ test/test_state.py
@@ -59,5 +59,5 @@ class TestState(unittest.TestCase):
         with self.assertRaises(ValueError):
             State(name="Test State", country="")
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
