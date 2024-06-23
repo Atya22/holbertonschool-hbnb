53e53d3ce5bec243dc233cb20ae08b5580c7a09c
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-23 10:35:00 +0000

    test_city.py

---
+++ test/test_city.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for City"""
+
+import unittest
+from datetime import datetime
+from model.city import City 
+
+class TestCity(unittest.TestCase):
+
+    def setUp(self):
+        self.city = City(name="New York", state="New York", place="Manhattan")
+
+    def test_initialization(self):
+        self.assertEqual(self.city.name, "New York")
+        self.assertEqual(self.city.state, "New York")
+        self.assertEqual(self.city.place, "Manhattan")
+        self.assertIsInstance(self.city.id, str)
+        self.assertIsInstance(self.city.update_time, datetime)
+        self.assertIsInstance(self.city.create_time, datetime)
+
+    def test_update(self):
+        self.city.update(name="Los Angeles", state="California")
+        self.assertEqual(self.city.name, "Los Angeles")
+        self.assertEqual(self.city.state, "California")
+        self.assertNotEqual(self.city.update_time, self.city.create_time)
+
+    def test_add_place(self):
+        initial_update_time = self.city.update_time
+        self.city.add_place("Brooklyn")
+        self.assertIn("Brooklyn", self.city.place)
+        self.assertNotEqual(self.city.update_time, initial_update_time)
