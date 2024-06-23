22a1f77a6192d040b9b7de41a31c1aae2431b7bf
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-23 10:29:49 +0000

    test_country.py

--- test/test_country.py
+++ test/test_country.py
@@ -34,10 +34,14 @@ class TestCountry(unittest.TestCase):
         self.country.remove_state("State1")
         self.assertEqual(self.country.states, initial_states)
 
+    def test_update_name(self):
+        self.country.update("NewTestland")
+        self.assertEqual(self.country.name, "NewTestland")
+
     def test_str_representation(self):
         self.country.add_state("State1")
         self.country.add_state("State2")
         expected_str = f"Country(ID: {self.country.id}, Name: Testland, States: [State1, State2])"
         self.assertEqual(str(self.country), expected_str)
-        if __name__ == '__main__':
-            unittest.main()
+if __name__ == '__main__':
+    unittest.main()
